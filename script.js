9166a11a5cc92d2417272e42ceb99561fd4638dc
juhlinhaah <145770946+juhlinhaah@users.noreply.github.com>
2025-08-28 11:26:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const caixaAlternativas = document.querySelector(".caixa-alternativa);
 const caixaResultado = document.querySelector(".caixa-resultado);
 const caixaResultado = document.querySelector(".caixa-resultado):
 
+  const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      "Alternativa 1",
+      "Alternativa 2 ",
+      ]
+  },
+  {
+
 
 
 
