c5a500351b51355c445abb1f196431e08e655185
juhlinhaah <145770946+juhlinhaah@users.noreply.github.com>
2025-08-28 11:21:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const caixaPrincipal = document.querySelector(".caixa-principal);
+  const caixaPerguntas = document.querySelector(".caixa-perguntas);
 const caixaAlternativas = document.querySelector(".caixa-alternativa);
 const caixaResultado = document.querySelector(".caixa-resultado);
 const caixaResultado = document.querySelector(".caixa-resultado):
+
 
 
 
