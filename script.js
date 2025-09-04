25cd9cf289426e1a1427bd5117e19c15b16f35d8
juhlinhaah <145770946+juhlinhaah@users.noreply.github.com>
2025-09-04 11:43:28 -0300

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal);
   const caixaPerguntas = document.querySelector(".caixa-perguntas);
 const caixaAlternativas = document.querySelector(".caixa-alternativa);
 const caixaResultado = document.querySelector(".caixa-resultado);
-const caixaResultado = document.querySelector(".caixa-resultado):
+const caixaResultado = document.querySelector(".caixa-resultado);
 
   const perguntas = [
   {
