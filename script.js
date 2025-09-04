834f65bfe383043e62e4d2aca6498dbabbfe44bd
juhlinhaah <145770946+juhlinhaah@users.noreply.github.com>
2025-09-04 11:36:15 -0300

    a

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const caixaResultado = document.querySelector(".caixa-resultado):
 
   const perguntas = [
   {
-    enunciado: 'Pergunta 1'
-    "A IA será capaz de criar soluções inovadoras para problemas
+    enunciado:"A IA será capaz de criar soluções inovadoras para problemas
  ambientais complexos?"
+  
       alternativas: {
 
       'Alternativa 1 sim',
