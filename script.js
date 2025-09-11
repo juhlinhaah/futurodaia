7c18ae8b00cbee927891c0e503e45ba1d8eac8b6
juhlinhaah <145770946+juhlinhaah@users.noreply.github.com>
2025-09-11 11:51:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal);
   const caixaPerguntas = document.querySelector(".caixa-perguntas);
 const caixaAlternativas = document.querySelector(".caixa-alternativa);
 const caixaResultado = document.querySelector(".caixa-resultado);
-const caixaResultado = document.querySelector(".caixa-resultado);
+const textoResultado = document.querySelector(".caixa-resultado);
 
   const perguntas = [
   {
@@ -90,6 +90,7 @@ atual++;
 
       
     
+
 
 
 
